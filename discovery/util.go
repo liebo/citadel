5651d9bad8f1ccef03e0de55c920b539d10c5a43
Victor Vieux <vieux@docker.com>
2014-10-13 18:23:54 +0000

    docker pkg WIP

--- discovery/util.go
+++ discovery/util.go
@@ -8,8 +8,8 @@ import (
 )
 
 // FetchSlaves returns the slaves for the discovery service at the specified endpoint
-func FetchSlaves(endpoint, username, cluster string) ([]string, error) {
-	resp, err := http.Get(fmt.Sprintf("%s/u/%s/%s", endpoint, username, cluster))
+func FetchSlavesRaw(url string) ([]string, error) {
+	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
@@ -26,8 +26,20 @@ func FetchSlaves(endpoint, username, cluster string) ([]string, error) {
 	return addrs, nil
 }
 
+// FetchSlaves returns the slaves for the discovery service at the specified endpoint
+func FetchSlaves(endpoint, username, cluster string) ([]string, error) {
+	return FetchSlavesRaw(fmt.Sprintf("%s/u/%s/%s", endpoint, username, cluster))
+}
+
 // RegisterSlave adds a new slave identified by the slaveID into the discovery service
 // the default TTL is 30 secs
+func RegisterSlaveRaw(url, slaveID, addr string) error {
+	buf := strings.NewReader(addr)
+
+	_, err := http.Post(fmt.Sprintf("%s/%s", url, slaveID), "application/json", buf)
+	return err
+}
+
 func RegisterSlave(endpoint, username, cluster, slaveID, addr string) error {
 	buf := strings.NewReader(addr)
 
--- examples/main.go
+++ examples/main.go
@@ -1,99 +1,25 @@
 package main
 
 import (
-	"crypto/md5"
-	"fmt"
 	"log"
-	"time"
 
-	"github.com/citadel/citadel"
-	"github.com/citadel/citadel/api"
-	"github.com/citadel/citadel/cluster"
-	"github.com/citadel/citadel/discovery"
-	"github.com/citadel/citadel/scheduler"
+	"github.com/citadel/citadel/docker"
 )
 
-// temporary register 2 nodes
-func registerTestSlaves() {
-	// Ubuntu boxes
-	discovery.RegisterSlave("http://discovery.crosbymichael.com", "citadel_test", "cluster", "node1", "http://ec2-54-68-133-155.us-west-2.compute.amazonaws.com:4242")
-	discovery.RegisterSlave("http://discovery.crosbymichael.com", "citadel_test", "cluster", "node2", "http://ec2-54-69-225-30.us-west-2.compute.amazonaws.com:4242")
-	// Fedora
-	discovery.RegisterSlave("http://discovery.crosbymichael.com", "citadel_test", "cluster", "node3", "http://ec2-54-69-11-29.us-west-2.compute.amazonaws.com:4242")
-}
-
 func main() {
+	// temporary register 3 nodes
 
-	go func() {
-		for {
-			time.Sleep(25 * time.Second)
-			registerTestSlaves()
-		}
-	}()
-
-	registerTestSlaves()
-	nodes, err := discovery.FetchSlaves("http://discovery.crosbymichael.com", "citadel_test", "cluster")
-	if err != nil {
+	// Ubuntu boxes
+	if err := docker.Slave("http://discovery.crosbymichael.com/u/citadel_test/cluster", "node1", "http://ec2-54-68-133-155.us-west-2.compute.amazonaws.com:4242"); err != nil {
+		log.Fatal(err)
+	}
+	if err := docker.Slave("http://discovery.crosbymichael.com/u/citadel_test/cluster", "node2", "http://ec2-54-69-225-30.us-west-2.compute.amazonaws.com:4242"); err != nil {
+		log.Fatal(err)
+	}
+	// Fedora
+	if err := docker.Slave("http://discovery.crosbymichael.com/u/citadel_test/cluster", "node3", "http://ec2-54-69-11-29.us-west-2.compute.amazonaws.com:4242"); err != nil {
 		log.Fatal(err)
 	}
 
-	var engines []*citadel.Engine
-	for _, node := range nodes {
-		engine := citadel.NewEngine(fmt.Sprintf("node-%x", md5.Sum([]byte(node))), node, 2048, 1)
-		if err := engine.Connect(nil); err != nil {
-			log.Fatalf("node.Connect: %v", err)
-		}
-		engines = append(engines, engine)
-	}
-
-	c, err := cluster.New(scheduler.NewResourceManager(), 2*time.Second, engines...)
-	if err != nil {
-		log.Fatalf("cluster.New: %v", err)
-	}
-	defer c.Close()
-
-	scheduler := &scheduler.LabelScheduler{}
-
-	if err := c.RegisterScheduler("service", scheduler); err != nil {
-		log.Fatalf("c.RegisterScheduler: %v", err)
-	}
-
-	if err := c.Events(api.EventsHandler); err != nil {
-		log.Fatalf("c.Events: %v", err)
-	}
-
-	/*	for {
-			fmt.Println("")
-			for _, containers := range c.Containers {
-				for _, container := range containers {
-					fmt.Println(container)
-				}
-			}
-			time.Sleep(2 * time.Second)
-		}
-	*/
-	/*
-		image := &citadel.Image{
-			Name:   "redis",
-			Memory: 256,
-			Cpus:   0.4,
-			Type:   "service",
-		}
-
-		for i := 0; i < 2; i++ {
-			container, err := c.Start(image, true)
-			if err != nil {
-				log.Fatalf("c.Start: %v", err)
-			}
-
-			log.Printf("ran container %s\n", container.ID)
-		}
-		containers, err := c.ListContainers()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-	*/
-
-	log.Fatal(api.ListenAndServe(c, ":4243"))
+	log.Fatal(docker.Master("http://discovery.crosbymichael.com/u/citadel_test/cluster", ":4243"))
 }
